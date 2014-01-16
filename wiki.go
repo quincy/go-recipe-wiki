52bf5926789e80871aa8580fbf1e82f53422318a
Quincy Bowers <qbowers@clearwateranalytics.com>
2014-01-16 07:46:13 -0700

    Added some comments.

--- wiki.go
+++ wiki.go
@@ -17,26 +17,19 @@ import (
 	"github.com/russross/blackfriday"
 )
 
+// Page represents a single page in the wiki.
 type Page struct {
 	Title string
 	Body  template.HTML
 }
 
-var pagesDir string = "pages"
-
-func init() {
-	if _, err := os.Stat(pagesDir); os.IsNotExist(err) {
-		if err := os.Mkdir(pagesDir, 0700); err != nil {
-			panic(err)
-		}
-	}
-}
-
+// save writes the page out to disk.
 func (p *Page) save() error {
 	filename := filepath.Join(pagesDir, p.Title+".txt")
 	return ioutil.WriteFile(filename, []byte(p.Body), 0600)
 }
 
+// loadPage reads a page from disk.
 func loadPage(title string) (*Page, error) {
 	filename := filepath.Join(pagesDir, title+".txt")
 	body, err := ioutil.ReadFile(filename)
@@ -46,6 +39,8 @@ func loadPage(title string) (*Page, error) {
 	return &Page{Title: title, Body: template.HTML(body)}, nil
 }
 
+// viewHandler prepares the page to be rendered by passing it through the
+// markdown and wikiMarkup filters.
 func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
 	p, err := loadPage(title)
 	if err != nil {
@@ -59,6 +54,8 @@ func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
 	renderTemplate(w, "view", p)
 }
 
+// editHandler loads an existing page from disk or creates a new empty page to
+// be rendered.
 func editHandler(w http.ResponseWriter, r *http.Request, title string) {
 	p, err := loadPage(title)
 	if err != nil {
@@ -67,6 +64,7 @@ func editHandler(w http.ResponseWriter, r *http.Request, title string) {
 	renderTemplate(w, "edit", p)
 }
 
+// saveHandler saves the changes and redirects back to the page's view.
 func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
 	body := r.FormValue("body")
 	p := &Page{Title: title, Body: template.HTML(body)}
@@ -78,6 +76,7 @@ func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
 	http.Redirect(w, r, "/view/"+title, http.StatusFound)
 }
 
+// Parse the templates.
 var templateDir string = "templates"
 var templateFiles []string = []string{
 	filepath.Join(templateDir, "edit.html"),
@@ -85,6 +84,7 @@ var templateFiles []string = []string{
 
 var templates = template.Must(template.ParseFiles(templateFiles...))
 
+// renderTemplate takes the renders the html for the given template.
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 	err := templates.ExecuteTemplate(w, tmpl+".html", p)
 	if err != nil {
@@ -92,6 +92,7 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 	}
 }
 
+// Defines the set of valid URLs to expect.
 var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
 
 func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
@@ -108,14 +109,27 @@ func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.Handl
 // a wikiLink looks like [[Words]]
 var wikiLink = regexp.MustCompile("\\[\\[(Home)\\]\\]")
 
+// convertWikiMarkup replaces wiki syntax with equivalent html.
 func convertWikiMarkup(text []byte) []byte {
 	var resultText = wikiLink.ReplaceAll(text, []byte("<a href=\"/view/$1\">$1</a>"))
 	return resultText
 }
 
+// Ensure the pages directory exists before the program gets going.
+var pagesDir string = "pages"
+
+func init() {
+	if _, err := os.Stat(pagesDir); os.IsNotExist(err) {
+		if err := os.Mkdir(pagesDir, 0700); err != nil {
+			panic(err)
+		}
+	}
+}
+
 func main() {
 	var server = "localhost:8080"
 
+	// open the default browser to the view/Home endpoint.
 	var browser *exec.Cmd
 	var url string = "http://" + server + "/view/Home"
 
@@ -131,6 +145,7 @@ func main() {
 		panic(err)
 	}
 
+	// register the handlers and start the server.
 	http.HandleFunc("/view/", makeHandler(viewHandler))
 	http.HandleFunc("/edit/", makeHandler(editHandler))
 	http.HandleFunc("/save/", makeHandler(saveHandler))
